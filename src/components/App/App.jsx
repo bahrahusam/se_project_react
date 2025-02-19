a5562e3bebe421be1dd5181623d51c16241c1e4b
Husam Bahra <husamsrock@gmail.com>
2025-02-19 07:53:56 -0500

    new updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Header from "../Header/Header";
 function App() {
-  return <div className="app">Hello this is Husam Y Bahra, big Shmoney moves.</div>;
+  return (
+    <div className="page">
+      <div className="page__content">
+        Hello this is Husam Y Bahra, big Shmoney moves.
+      </div>
+    </div>
+  );
 }
 
 export default App;
