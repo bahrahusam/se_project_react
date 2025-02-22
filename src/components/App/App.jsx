02df00c1b6a31ebafd45fefa8e02419d2621a23b
Husam Bahra <115939378+bahrahusam@users.noreply.github.com>
2025-02-22 15:33:14 +0000

    new stuff

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import Header from "../Header/Header";
 function App() {
-  return (
-    <div className="page">
-      <div className="page__content">
-        Hello this is Husam Y Bahra, big Shmoney moves.
-      </div>
-    </div>
-  );
+  return <div className="page"> 
+  <div className="page__content"><Header /></div>
+  </div>;
 }
-
 export default App;
+
+
