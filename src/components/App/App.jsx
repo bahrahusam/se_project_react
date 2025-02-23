a65152c78630f973269251373ab483a40275686c
Husam Bahra <husamsrock@gmail.com>
2025-02-22 21:13:05 -0500

    new stuff 22FEB25

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 function App() {
-  return <div className="page"> 
-  <div className="page__content"><Header /></div>
-  </div>;
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main />
+      </div>
+    </div>
+  );
 }
 export default App;
-
-
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
-import "./Header.css"
+import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
-function Header(){
-  return <header className="header">
-      <img className= "header__logo" src={logo} />
-      <p className="header__date-location">Date, Location</p>
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-location">DATE, LOCATION</p>
       <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
       </div>
-  </header>;
+    </header>
+  );
 }
 export default Header;
