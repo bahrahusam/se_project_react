169e1037b76b368cd846d56e36b1f6eb931b6665
Husam Bahra <husamsrock@gmail.com>
2025-03-04 03:53:04 -0500

    new updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,58 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
-
+  const [activeModal, setActiveModal] = useState("");
   return (
     <div className="page">
       <div className="page__content">
         <Header />
         <Main weatherData={weatherData} />
       </div>
-      <ModalWithForm />
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name {""}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image {""}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__input-radio" />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="warm" type="radio" className="modal__input-radio" />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__input-radio" />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,29 +1,22 @@
-function ModalWithForm() {
-return ( <div className="modal"> 
+import "./ModalWithForm.css";
 
-<form className="modal__form">
-<h2 className="modal__title">New Garment</h2>
-<button type="button" className="modal__close">CLOSE</button>
-<label htmlFor="name" className="modal__label">
-    Name {""} 
-    <input type="text" 
-    className="modal__input" 
-    id="name" 
-    placeholder="Name" />
-</label>
-<label htmlFor="imageUrl" className="modal__label">
-    Image {""}
-     <input type="url" 
-     className="modal__input" 
-     id="imageUrl" 
-     placeholder="Image URL" />
-</label>
-<fieldset className="modal__radio-buttons">
-    
-</fieldset>
-</form>
-</div>
-    );
+function ModalWithForm({ children, buttonText, title, activeModal }) {
+  return (
+    <div
+      className={`modal ${activeModal === "add-garment" && "modal__opened"}`}
+    >
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button type="button" className="modal__close"></button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ModalWithForm;
